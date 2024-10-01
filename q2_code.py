89118f8c75f93725f984e173974784d3f070d501
Navini Jagoda <123384639+Navini11@users.noreply.github.com>
2024-10-01 19:53:28 +0530

    Update q2_code.py

--- q2_code.py
+++ q2_code.py
@@ -5,56 +5,45 @@ import matplotlib.pyplot as plt
 # Load the brain proton density image
 image_path = 'a1images/a1images/brain_proton_density_slice.png'
 brain_image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+## selected pixel ranges
+white_th = np.array([120,175], dtype=np.uint8)
+grey_th = np.array([180,240], dtype=np.uint8)
 
-# Function to apply intensity transformation to accentuate white/gray matter
-def accentuate_matter(image, matter='white'):
-    if matter == 'white':
-        # Apply a transformation to enhance white matter (e.g., intensify higher intensity values)
-        transformed_image = np.interp(image, [0, 100, 150, 255], [0, 50, 200, 255])
-    elif matter == 'gray':
-        # Apply a transformation to enhance gray matter (e.g., intensify middle intensity values)
-        transformed_image = np.interp(image, [0, 50, 120, 255], [0, 100, 180, 255])
-    return transformed_image
+white_matter_transform = np.zeros(256, dtype=np.uint8)
+white_matter_transform[white_th[0]:white_th[1]+1] = np.linspace(white_th[0], white_th[1],white_th[1]-white_th[0]+1,dtype=np.uint8)
 
-# Apply the transformations for both white and gray matter
-white_matter_image = accentuate_matter(brain_image, 'white')
-gray_matter_image = accentuate_matter(brain_image, 'gray')
+grey_matter_transform = np.zeros(256, dtype=np.uint8)
+grey_matter_transform[grey_th[0]:grey_th[1]+1] = np.linspace(grey_th[0], grey_th[1],grey_th[1]-grey_th[0]+1,dtype=np.uint8)
 
-# Display original image
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 3, 1)
-plt.imshow(brain_image, cmap='gray')
-plt.title('Original Brain Image')
+white_matter = cv2.LUT(brain_image, white_matter_transform)
+grey_matter = cv2.LUT(brain_image, grey_matter_transform)
 
-# Display white matter accentuated image
-plt.subplot(1, 3, 2)
-plt.imshow(white_matter_image, cmap='gray')
-plt.title('White Matter Accentuated')
+#plot intensity transform functions
+fig,ax = plt.subplots(1,2,figsize=(12,4))
 
-# Display gray matter accentuated image
-plt.subplot(1, 3, 3)
-plt.imshow(gray_matter_image, cmap='gray')
-plt.title('Gray Matter Accentuated')
+ax[0].plot(white_matter_transform, 'r')
+ax[0].set_title('White Matter Intensity Transform')
+ax[0].set_xlabel('Intensity Level')
+ax[0].set_ylabel('Pixel Value')
+ax[0].set_ylim([0,255])
 
-plt.show()
+ax[1].plot(grey_matter_transform, 'r')
+ax[1].set_title('Grey Matter Intensity Transform')
+ax[1].set_xlabel('Intensity Level')
+ax[1].set_ylabel('Pixel Value')
+ax[1].set_ylim([0,255])
 
-# Plot the intensity transformations
-plt.figure(figsize=(10, 5))
-# Plot for white matter
-plt.subplot(1, 2, 1)
-plt.plot(np.linspace(0, 255, len(np.interp(np.arange(0, 256), [0, 100, 150, 255], [0, 50, 200, 255]))),
-         np.interp(np.arange(0, 256), [0, 100, 150, 255], [0, 50, 200, 255]), color='blue')
-plt.title('Intensity Transformation for White Matter')
-plt.xlabel('Input Intensity')
-plt.ylabel('Output Intensity')
+#plot images
 
-# Plot for gray matter
-plt.subplot(1, 2, 2)
-plt.plot(np.linspace(0, 255, len(np.interp(np.arange(0, 256), [0, 50, 120, 255], [0, 100, 180, 255]))),
-         np.interp(np.arange(0, 256), [0, 50, 120, 255], [0, 100, 180, 255]), color='green')
-plt.title('Intensity Transformation for Gray Matter')
-plt.xlabel('Input Intensity')
-plt.ylabel('Output Intensity')
+fig,ax = plt.subplots(1,3,figsize=(12,4))
+
+ax[0].imshow(brain_image, cmap='gray')
+ax[0].set_title('Original Image')
+
+ax[1].imshow(white_matter, cmap='gray')
+ax[1].set_title('White Matter')
+
+ax[2].imshow(grey_matter, cmap='gray')
+ax[2].set_title('Grey Matter')
 
 plt.show()
-
